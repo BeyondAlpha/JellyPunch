140fb08420f4539de23fae7912d3792df9f3ea71
Karen Kim <kyoun5546@gmail.com>
2020-11-26 22:35:17 +0900

    Fixed bug

---
+++ Assets/Script/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    void Start()
+    {
+        this.player = GameObject.Find("Nabi");
+    }
+
+    void Update()
+    {
+        Vector3 playerPos = this.player.transform.position;
+        transform.position = new Vector3(
+            transform.position.x, playerPos.y, transform.position.z);
+        )   
+    }
+}
